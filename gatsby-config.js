016437e615f4a3b34ddde9cbf7e3cf97b3f981e4
Benedikt Rötsch <axe312ger@users.noreply.github.com>
2022-10-21 10:02:28 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,12 +13,12 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     'gatsby-transformer-remark',
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
-    },
+  //  {
+  //    resolve: `gatsby-plugin-typography`,
+  //    options: {
+  //      pathToConfigModule: `src/utils/typography.js`
+  //    }
+  //  },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
